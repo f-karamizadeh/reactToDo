3ceac0554675c765a2764fa0faf5b6829a47b817
Ben84FFM <149787249+Ben84FFM@users.noreply.github.com>
2023-12-14 09:34:11 +0100

    Ben

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import AddTodo from './components/AddTodo.jsx'
-import DisplayTodo from './components/DisplayTodo.jsx'
+
 // Benötigte Komponenten
 export default function App() {
     return (
         <>
         <AddTodo />
-        <DisplayTodo />
         </>
     );
 }
